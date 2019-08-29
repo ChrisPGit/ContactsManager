b960dee47a4a8c7c1e82728a9ad35b725e5c706c
christophe.poty <christophe.poty@hotmail.com>
2019-08-29 20:00:59 +0200

    launch

---
+++ ContactsManager.API/Controllers/ReadyController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace ContactsManager.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ReadyController : ControllerBase
+    {
+        // GET api/values
+        [HttpGet]
+        public ActionResult<string> Get()
+        {
+            return $"The API is ready. {DateTime.Now.ToLongDateString()} à {DateTime.Now.ToLongTimeString()}";
+        }
+    }
+}
